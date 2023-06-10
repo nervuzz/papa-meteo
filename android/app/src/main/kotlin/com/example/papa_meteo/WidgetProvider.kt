737022d20539f504a71525cb7dca837ac77963dc
nervuzz <nervuzz@example.com>
2023-06-11 00:07:32 +0200

    Home widget works as expected

--- android/app/src/main/kotlin/com/example/papa_meteo/WidgetProvider.kt
+++ android/app/src/main/kotlin/com/example/papa_meteo/WidgetProvider.kt
@@ -3,6 +3,7 @@ package com.nervuzz.papa_meteo // your package name
 import android.os.Bundle
 import android.graphics.Bitmap
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import android.graphics.drawable.Drawable
 import android.appwidget.AppWidgetManager
 import android.content.Context
@@ -10,7 +11,6 @@ import android.content.SharedPreferences
 import android.net.Uri
 import android.widget.RemoteViews
 import es.antonborri.home_widget.HomeWidgetBackgroundIntent
-import es.antonborri.home_widget.HomeWidgetLaunchIntent
 import es.antonborri.home_widget.HomeWidgetProvider
 
 class HomeScreenWidgetProvider : HomeWidgetProvider() {
@@ -18,40 +18,31 @@ class HomeScreenWidgetProvider : HomeWidgetProvider() {
      override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray, widgetData: SharedPreferences) {
         appWidgetIds.forEach { widgetId ->
             val views = RemoteViews(context.packageName, R.layout.widget_layout).apply {
-                // Open App on Widget Click
-                val pendingIntent = HomeWidgetLaunchIntent.getActivity(
-                        context,
-                        MainActivity::class.java)
-                setOnClickPendingIntent(R.id.widget_container, pendingIntent)
+                // Set current city label
+                val city = widgetData.getString("city", null)
+                setTextViewText(R.id.widget_city, city?: "No city set")
 
-                // Swap Title Text by calling Dart Code in the Background
-                // setTextViewText(R.id.widget_title, widgetData.getString("title", null)?: "No Title Set")
-                // val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(
-                //         context,
-                //         Uri.parse("homeWidgetExample://titleClicked")
-                // )
-                // setOnClickPendingIntent(R.id.widget_title, backgroundIntent)
+                // Set current datetime label
+                val dt = widgetData.getString("dt", null)
+                setTextViewText(R.id.widget_dt, dt?: "Pending refresh..")
 
-                val message = widgetData.getString("message", null)
-                setTextViewText(R.id.widget_message, message?: "No Message Set")
-
-                // // Detect App opened via Click inside Flutter
-                // val pendingIntentWithData = HomeWidgetLaunchIntent.getActivity(
-                //         context,
-                //         MainActivity::class.java,
-                //         Uri.parse("homeWidgetExample://message?message=$message"))
-                // setOnClickPendingIntent(R.id.widget_message, pendingIntentWithData)
-
+                // Set current forecast image
                 val imageURL = widgetData.getString("widgetImg", "https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/spitzer20180822-nasa-small.jpg")
-
-                Picasso.get().load(imageURL).into(object : com.squareup.picasso.Target {
+                Picasso.get().load(imageURL).into(object : Target {
                         override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {}
                         override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
-                                // loaded bitmap is here (bitmap)
+                                // Downloaded bitmap goes here
                                 setImageViewBitmap(R.id.widgetImg, bitmap)
                         }
                         override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
-                        })
+                })
+
+                // Update forecast by click on City label
+                val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(
+                        context,
+                        Uri.parse("homeScreenWidget://cityClicked")
+                )
+                setOnClickPendingIntent(R.id.widget_city, backgroundIntent)  
             }
 
             appWidgetManager.updateAppWidget(widgetId, views)
